cc4e03bbeefb1552d4682e61ea95adea85fc9125
Fan Jiang <prof.fan@foxmail.com>
2016-02-26 21:30:33 +0800

    Fixed local rendering and added submodule

--- test.js
+++ test.js
@@ -12,12 +12,20 @@ var page = webPage.create();
        background-image: background-image: url('http://retinajs.s3.amazonaws.com/images/backgrounds/bg@2x.jpg');
     }
 } */
-var examplePage = 'http://angularplasmid.vixis.com/samples.php';
+var examplePage = 'file:///Users/proffan/Projects/render2plasmid/angularplasmid/examples/main/pPMA43C.html';
 
 //Pass a dpr of your choosing
 page.zoomFactor = 4; // in this case, >=1.5 to get the retina-supporting bg-image
 
-page.open(examplePage, function() {
+page.onResourceRequested = function(requestData, networkRequest) {
+  console.log('Request (#' + requestData.id + '): ' + JSON.stringify(requestData));
+};
+
+page.onResourceReceived = function(response) {
+  console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + JSON.stringify(response));
+};
+
+page.open(examplePage, function(status) {
    page.render('2x.png');
    phantom.exit();
 });
